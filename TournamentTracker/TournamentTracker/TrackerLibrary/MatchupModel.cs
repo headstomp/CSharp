8cda207d57473768c1206d00b5310b4d8ab1e0e4
headstomp <pkazmir@gmail.com>
2018-07-08 19:39:59 -0400

    Class library setup

---
+++ TournamentTracker/TournamentTracker/TrackerLibrary/MatchupModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackerLibrary
+{
+    public class MatchupModel
+    {
+        /// <summary>
+        /// Represents the matched teams 
+        /// </summary>
+        public List<MatchupModel> Entries { get; set; } = new List<MatchupModel>();
+        public TeamModel Winner { get; set; }
+        public int MatchupRound { get; set; }
+    }
+}

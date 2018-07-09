1e0b7a217a5a76fb54999cc37228929d3e17c9f2
headstomp <pkazmir@gmail.com>
2018-07-09 04:09:00 -0400

    Playing with WebApi

---
+++ WebApiSetup/DemoApi/Controllers/PeopleController.cs
@@ -0,0 +1,70 @@
+﻿using DemoApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace DemoApi.Controllers
+{
+    /// <summary>
+    /// This is where we comment on the People Controller
+    /// </summary>
+    public class PeopleController : ApiController
+    {
+        List<Person> people = new List<Person>();
+
+        public PeopleController()
+        {
+            people.Add(new Person { FirstName = "Paul", LastName = "Kazmir", Id = 1 });
+            people.Add(new Person { FirstName = "Sue", LastName = "Storm", Id = 2 });
+            people.Add(new Person { FirstName = "Joe", LastName = "Smith", Id = 3 });
+        }
+
+        /// <summary>
+        /// Get list of first names
+        /// </summary>
+        /// <returns></returns>
+        [Route("api/People/GetFirstNames")]
+        [HttpGet]
+        public List<string> GetFirstNames()
+        {
+            List<string> output = new List<string>();
+
+            foreach (var p in people)
+            {
+                output.Add(p.FirstName);
+            }
+            return output;
+        }
+
+        // GET: api/People
+        public List<Person> Get()
+        {
+            return people;
+        }
+
+        // GET: api/People/5
+        public Person Get(int id)
+        {
+            return people.Where(x => x.Id == id).FirstOrDefault();
+        }
+
+        // POST: api/People
+        public void Post(Person val)
+        {
+            people.Add(val);
+        }
+
+        // PUT: api/People/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/People/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ WebApiSetup/DemoApi/Models/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DemoApi.Models
+{
+    /// <summary>
+    /// Represents one specific person
+    /// </summary>
+    public class Person
+    {
+        /// <summary>
+        /// id form SQL
+        /// </summary>
+        public int Id { get; set; } = 0;
+
+        /// <summary>
+        /// Persons first name
+        /// </summary>
+        public string FirstName { get; set; } = "";
+
+        /// <summary>
+        /// Persons last name
+        /// </summary>
+        public string LastName { get; set; } = "";
+    }
+}

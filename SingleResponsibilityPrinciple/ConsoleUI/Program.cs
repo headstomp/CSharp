5fe4ad722bed6584ce12b752f335986df7859556
headstomp <pkazmir@gmail.com>
2018-07-31 16:49:12 -0400

    initial

--- SingleResponsibilityPrinciple/ConsoleUI/Program.cs
+++ SingleResponsibilityPrinciple/ConsoleUI/Program.cs
@@ -1,26 +1,41 @@
 ﻿
 
+using System;
+
 namespace ConsoleUI
 {
     class Program
     {
         static void Main(string[] args)
         {
-            StandardMessages.WelcomeMessage();
+            Console.WriteLine("Welcome to my application");
 
-            Person user = PersonDataCapture.Capture();
+            //Ask for user information
+            Person user = new Person();
 
-            bool isUserValid = PersonValidator.Validate(user);
+            Console.Write("What is your first name: ");
+            user.FirstName = Console.ReadLine();
 
-            if (isUserValid == false)
+            Console.Write("What is your last name: ");
+            user.LastName = Console.ReadLine();
+
+            // Checks to be sure the first and last names are valid
+            if (string.IsNullOrWhiteSpace(user.FirstName))
             {
-                StandardMessages.EndApplication();
+                Console.WriteLine("You did not givee a valid first name");
+                Console.ReadLine();
+                return;
+            }
+            if (string.IsNullOrWhiteSpace(user.LastName))
+            {
+                Console.WriteLine("You did not givee a valid last name");
+                Console.ReadLine();
                 return;
             }
 
-            AccountGenerator.CreateAccount(user);
-
-            StandardMessages.EndApplication();
+            //Create a username for the person
+            Console.WriteLine($"Your username is { user.FirstName.Substring(0,1)}{ user.LastName }");
+            Console.ReadLine();
         }
     }
 }

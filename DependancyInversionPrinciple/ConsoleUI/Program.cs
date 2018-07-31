332e80010b8122240a16a45ea1121ede2f42eefd
headstomp <pkazmir@gmail.com>
2018-07-30 20:18:56 -0400

    added interfaces, centralized new up instances into factory, added Texter class with DI

--- DependancyInversionPrinciple/ConsoleUI/Program.cs
+++ DependancyInversionPrinciple/ConsoleUI/Program.cs
@@ -11,19 +11,16 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
-            Person owner = new Person
-            {
-                FirstName = "Paul",
-                LastName = "Kazmir",
-                EmailAddress = "pk@gmail.com",
-                PhoneNumber = "555-1212"
-            };
+            IPerson owner = Factory.CreatePerson();
 
-            Chore chore = new Chore
-            {
-                ChoreName = "Take out the trash",
-                Owner = owner
-            };
+            owner.FirstName = "Paul";
+            owner.LastName = "Kazmir";
+            owner.EmailAddress = "pk@gmail.com";
+            owner.PhoneNumber = "555-1212";
+
+            IChore chore = Factory.CreateChore();
+            chore.ChoreName = "Take out the trash";
+            chore.Owner = owner;
 
             chore.PerformedWork(3);
             chore.PerformedWork(1.5);

0c7e049ed3893395723605991f2433f9321b3297
headstomp <pkazmir@gmail.com>
2018-07-31 20:37:16 -0400

    refactored using ISP

--- InterfaceSegregationPrinciple/ConsoleUI/Program.cs
+++ InterfaceSegregationPrinciple/ConsoleUI/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DemoLibrary;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,9 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
+            IBorrowableDVD dvd = new DVD();
+
+
 
             Console.ReadLine();
         }

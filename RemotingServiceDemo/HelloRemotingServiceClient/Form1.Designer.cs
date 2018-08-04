e7a7975ecaa2f2e1e2f61334c932e5cde283f83c
headstomp <pkazmir@gmail.com>
2018-08-03 23:34:45 -0400

    remoting test

---
+++ RemotingServiceDemo/HelloRemotingServiceClient/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace HelloRemotingServiceClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.buttonGetMessage = new System.Windows.Forms.Button();
+            this.labelMessage = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(48, 32);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(100, 20);
+            this.textBoxName.TabIndex = 0;
+            // 
+            // buttonGetMessage
+            // 
+            this.buttonGetMessage.Location = new System.Drawing.Point(178, 29);
+            this.buttonGetMessage.Name = "buttonGetMessage";
+            this.buttonGetMessage.Size = new System.Drawing.Size(122, 23);
+            this.buttonGetMessage.TabIndex = 1;
+            this.buttonGetMessage.Text = "Get Message";
+            this.buttonGetMessage.UseVisualStyleBackColor = true;
+            this.buttonGetMessage.Click += new System.EventHandler(this.buttonGetMessage_Click);
+            // 
+            // labelMessage
+            // 
+            this.labelMessage.AutoSize = true;
+            this.labelMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelMessage.Location = new System.Drawing.Point(48, 77);
+            this.labelMessage.Name = "labelMessage";
+            this.labelMessage.Size = new System.Drawing.Size(0, 25);
+            this.labelMessage.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(379, 202);
+            this.Controls.Add(this.labelMessage);
+            this.Controls.Add(this.buttonGetMessage);
+            this.Controls.Add(this.textBoxName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.Button buttonGetMessage;
+        private System.Windows.Forms.Label labelMessage;
+    }
+}
+
---
+++ RemotingServiceDemo/HelloRemotingServiceClient/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Tcp;
+
+namespace HelloRemotingServiceClient
+{
+    public partial class Form1 : Form
+    {
+        IHelloRemotingService.IHelloRemotingService client;
+
+        public Form1()
+        {
+            InitializeComponent();
+            TcpChannel channel = new TcpChannel();
+            ChannelServices.RegisterChannel(channel);
+            client = (IHelloRemotingService.IHelloRemotingService)Activator.GetObject
+                (typeof(IHelloRemotingService.IHelloRemotingService), "tcp://localhost:8080/GetMessage");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonGetMessage_Click(object sender, EventArgs e)
+        {
+            labelMessage.Text = client.GetMessage(textBoxName.Text);
+        }
+    }
+}
